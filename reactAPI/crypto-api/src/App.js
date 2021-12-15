a1f38f8b9967670db2f7382f02d645eeaaa3bbc9
saeedghorbanim <sghorbani123@yahoo.com>
2021-12-15 00:19:10 -0600

    updates have been made

---
+++ reactAPI/crypto-api/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello there</h1>
+      <button className="btn btn-success">Show me the money</button>
+    </div>
+  );
+}
+
+export default App;
--- reactFunctionalComponents/wall-of-ninjas/src/App.js
+++ reactFunctionalComponents/wall-of-ninjas/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import NinjaForm from './components/NinjaForm';
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div className="App container">
+      <h1>This is the Wall of Ninjas</h1>
+      <NinjaForm></NinjaForm>
     </div>
   );
 }
