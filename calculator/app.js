cd1727f33ff470a817b5d19cadc3dabc33696a0a
saeedghorbanim <sghorbani123@yahoo.com>
2022-01-08 13:43:54 -0600

    obtaining input from user

---
+++ calculator/app.js
@@ -0,0 +1,23 @@
+const prompt = require('prompt-sync')({sigint:true});
+var num = prompt("> ");
+let stack = [num];
+
+
+console.log(stack[0]);
+
+
+
+
+// const reversePN = (arr) => {
+
+//     // const prompt = require('prompt-sync')({sigint:true});
+//     // var num = prompt("> ");
+//     // arr.push = num;
+//     for(let i = 0; i < 10; i++)
+//     {
+//         console.log(arr[i]);
+//     }
+// }
+
+// reversePN(stack)
+
